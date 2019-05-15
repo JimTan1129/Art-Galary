753b10d23bb320ee93afbce54e1db7987887594e
Jimmy Tan <tanjimmy1129@gmail.com>
2019-05-15 17:00:17 -0400

    added render

---
+++ main.js
@@ -0,0 +1,15 @@
+class Classic {
+  constructor(art_url) {
+    this.frame = 'black-border'
+    this.art_url ? art_url : 'default_url'
+  }
+// thing_to_set = condition ? 
+
+render() {
+  let el = document.createElement('div')
+  el.style.width = '100px'
+  el.style.height = '100px'
+  el.style.background = 'black'
+  document.body.appendChild(hl)
+}
+}
